112314a429e59d374785c19b30e580a975f90b75
balaban <balaban.alexander@gmail.com>
2019-01-10 20:54:55 +0000

    Initial Commit

---
+++ time-tracker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  time-tracker
+//
+//  Created by Alexander Balaban on 10/01/2019.
+//  Copyright © 2019 Balaban. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
